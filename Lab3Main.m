b17020e8bf95938667e285f52d0506defc8606c8
alext462 <atilbury8@gmail.com>
2025-04-21 08:56:38 -0600

    main code

---
+++ Lab3Main.m
@@ -0,0 +1,85 @@
+clc
+clear
+close all
+
+% Preallocate vectors
+
+    amplitude=0.5; 
+    
+
+    Kptheta = [10 20 5 10 10 10 ];
+    
+    Kdtheta = [0 0 0 1 -1 -0.5];
+   
+    
+[x, t]=CSYSTEMstep(Kptheta, Kdtheta, amplitude);
+
+[x2, t2]=CSYSTEMlsim(Kptheta, Kdtheta, amplitude)
+
+%% Functions 
+
+% 1
+function [x, t]=CSYSTEMstep(Kptheta, Kdtheta, amplitude)
+Kg = 33.3;
+Km = 0.0401;
+J = 0.0005 + 0.2 * (0.2794)^2 + 0.0015;
+Rm = 19.2;
+
+for i=1:length(Kdtheta)
+n1 = Kptheta(i) * Kg * Km / (J * Rm);
+    d2 = 1;
+    d1 = Kg^2 * Km^2 / (J * Rm) + Kdtheta(i)* Kg* Km / (J * Rm);
+    d0 = Kptheta(i) * Kg * Km / (J * Rm);
+% Define transfer function
+    num = n1;
+    den = [d2 d1 d0];
+    sysTF = tf(num, den);
+    % Step Response
+    [x,t] = step(sysTF);
+x=x*amplitude; 
+figure(i)
+grid on
+plot(t, x)
+titlename=['Step Set ' num2str(i)];
+title(titlename)
+xlabel('Time (s)')
+ylabel('Theta (rad)')
+PlotName = ['Step Set ' num2str(i)];
+print(PlotName,'-dpng','-r300')
+end 
+
+end 
+
+% 2
+function [x, t]=CSYSTEMlsim(Kptheta, Kdtheta, amplitude)
+Kg = 33.3;
+Km = 0.0401;
+J = 0.0005 + 0.2 * (0.2794)^2 + 0.0015;
+Rm = 19.2;
+
+for i=1:length(Kdtheta)
+n1 = Kptheta(i) * Kg * Km / (J * Rm);
+    d2 = 1;
+    d1 = Kg^2 * Km^2 / (J * Rm) + Kdtheta(i)* Kg* Km / (J * Rm);
+    d0 = Kptheta(i) * Kg * Km / (J * Rm);
+% Define transfer function
+    num = n1;
+    den = [d2 d1 d0];
+    sysTF = tf(num, den);
+    % Step Response
+    t=0:0.01:10;
+    u=ones(1,length(t));
+    [x,t] = lsim(sysTF, u, t);
+x=x*amplitude; 
+figure(i)
+grid on
+plot(t, x)
+titlename=['Lsim Set ' num2str(i)];
+title(titlename)
+xlabel('Time (s)')
+ylabel('Theta (rad)')
+PlotName = ['Lsim Set ' num2str(i)];
+print(PlotName,'-dpng','-r300')
+end 
+
+end 
