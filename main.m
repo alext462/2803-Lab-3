98b9617432d08ac4f01c708d15f84ea7bc66ff31
alext462 <atilbury8@gmail.com>
2025-04-27 22:10:52 -0600

    updated plots and word doc

--- main.m
+++ main.m
@@ -15,29 +15,30 @@ time1 = (time1 - min(time1)) / 1000 + 2;
 time3 = (time3 - min(time3)) / 1000 + 2 + 1.1; % additional +1 second shift
 
 % Keep only values where time <= 6 seconds
-mask1 = time1 <= 6;
-mask3 = time3 <= 6;
+% mask1 = time1 <= 6;
+% mask3 = time3 <= 6;
 
-time1 = time1(mask1);
-value1 = value1(mask1);
+% time1 = time1(mask1);
+% value1 = value1(mask1);
+% 
+% time3 = time3(mask3);
+% value3 = value3(mask3);
 
-time3 = time3(mask3);
-value3 = value3(mask3);
-
-% Plot 1: K1 = 17, K3 = 1
-figure;
+%% Plot 1: K1 = 17, K3 = 1 for 2.4e
+figure(1);
 plot(time1, value1, 'b-', 'LineWidth', 1.5); hold on;
-plot([0 5], [0.5 0.5], '--r'); % Reference line +0.5
-plot([5 10], [-0.5 -0.5], '--b'); % Reference line -0.5
+plot([0 5], [0.5 0.5], '--k'); % Reference line +0.5
+plot([5 10], [-0.5 -0.5], '--k'); % Reference line -0.5
 plot([5 5], [0.5 -0.5], '--k'); % Transition line
 xlabel('Time (s)');
 ylabel('Measured Value');
 title('K_1 = 17, K_3 = 1');
-xlim([2, 6]);
+% xlim([2, 6]);
 grid on;
+print('17 and 1 Hardware', '-dpng', '-r300')
 
-% Plot 2: K1 = 7, K3 = 1
-figure;
+%% Plot 2: K1 = 7, K3 = 1 for 2.4c
+figure(2);
 plot(time3, value3, 'g-', 'LineWidth', 1.5); hold on;
 plot([0 5], [0.5 0.5], '--r'); % Reference line +0.5
 plot([5 10], [-0.5 -0.5], '--b'); % Reference line -0.5
@@ -45,5 +46,6 @@ plot([5 5], [0.5 -0.5], '--k'); % Transition line
 xlabel('Time (s)');
 ylabel('Measured Value');
 title('K_1 = 7, K_3 = 1');
-xlim([3, 6]); % Adjusted x-limits since data shifted by +3
+% xlim([3, 6]); % Adjusted x-limits since data shifted by +3
 grid on;
+print('7 and 1 Hardware', '-dpng', '-r300')
